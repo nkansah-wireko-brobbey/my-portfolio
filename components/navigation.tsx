c190e2cc6ee5fbce982e3fb0531cc73176f1913c
nkansah-wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-07-12 17:29:32 -0500

    chore: Add dark theme switch component and update navigation

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import Link from 'next/link'
+import {Button} from '@/components/ui/button'
+import { MountainIcon } from 'lucide-react'
+import { ModeToggle } from './dark-theme-switch'
 
-const navigation = () => {
+const Navigation = () => {
   return (
-    <div>navigation</div>
+    <header className="fixed top-0 left-0 z-50 w-full bg-background/90 backdrop-blur-md">
+        <div className="container mx-auto flex h-16 max-w-7xl items-center justify-between px-4 md:px-6">
+          <Link href="#" className="flex items-center gap-2" prefetch={false}>
+            <MountainIcon className="h-6 w-6" />
+            <span className="text-md font-semibold">Nkansah.</span>
+          </Link>
+          <nav className="hidden space-x-4 md:flex">
+            <Link href="#" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
+              Introduction
+            </Link>
+            <Link href="#" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
+              Skillset
+            </Link>
+            <Link href="#" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
+              Services
+            </Link>
+            <Link href="#" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
+              Portfolio
+            </Link>
+            <Link href="#" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
+              Contact
+            </Link>
+          </nav>
+          <div className='flex gap-4'>
+          <Button>{"Let's Get In Touch"}</Button>
+          <ModeToggle />
+
+          </div>
+        </div>
+      </header>
   )
 }
 
-export default navigation
+export default Navigation
