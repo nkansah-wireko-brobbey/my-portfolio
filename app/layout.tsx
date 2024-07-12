bdd284be811b5fceda2b510e866711017a9699cd
nkansah-wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-07-12 16:15:05 -0500

    chore: Add dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
-
+import { ThemeProvider } from "@/components/theme-provider";
 import { cn } from "@/lib/utils"
  
 const fontSans = FontSans({
@@ -25,7 +25,16 @@ export default function RootLayout({
           "min-h-screen bg-background font-sans antialiased",
           fontSans.variable
         )}
-      >{children}</body>
+      >
+     <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+      </body>
     </html>
   );
 }
