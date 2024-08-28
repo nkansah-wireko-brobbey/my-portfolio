355fa857d5b278bd8ea803ba31da578c15509d7c
nkansah-wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-08-27 23:04:24 -0500

    feat: Added project metadata

---
+++ constants/projects.ts
@@ -0,0 +1,63 @@
+
+const projects = [
+  {
+    title: "Weather App/Angular Signals",
+    description:
+      "This Simple Weather App showcases Angular Signals, offering beautifully designed components that you can copy and paste into your apps.",
+    gitHubLink: "https://github.com/nkansah-wireko-brobbey/my-weather-app",
+    liveDemoLink: "https://my-weather-app-alpha-six.vercel.app/home",
+    technologies: [
+      {
+        title: "Angular",
+        icon: "",
+      },
+      {
+        title: "TypeScript",
+        icon: "",
+      },
+      {
+        title: "rxjs",
+        icon: "",
+      },
+      {
+        title: "scss",
+        icon: "",
+      },
+    ],
+  },
+  {
+    title: "Message App / Angular - SocketIO",
+    description:
+      "A Message App built with Angular and SocketIO, featuring beautifully designed components that you can copy and paste into your apps.",
+    gitHubLink: "https://github.com/nkansah-wireko-brobbey/msg-app",
+    liveDemoLink: "https://msg-app-eight.vercel.app/",
+    technologies: [
+      {
+        title: "Angular",
+        icon: "",
+      },
+      {
+        title: "TypeScript",
+        icon: "",
+      },
+      {
+        title: "rxjs",
+        icon: "",
+      },
+      {
+        title: "scss",
+        icon: "",
+      },
+      {
+        title: "socketIO",
+        icon: "",
+      },
+      {
+        title: "jwt",
+        icon:""
+      }
+    ],
+  },
+];
+
+export default projects;
